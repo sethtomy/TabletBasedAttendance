229e0b8212ada832a7b9585258781a761672e9a7
Roger Williams <rogerwilliams365@gmail.com>
2019-02-23 14:58:26 -0500

    Created backend folder.

---
+++ backend/app/src/main/java/app/com/eattendance/SplashActivity.java
@@ -0,0 +1,26 @@
+package app.com.eattendance;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // 3 second delay before redirecting to the login activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // redirect to the login activity and close this activity
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+
+}
