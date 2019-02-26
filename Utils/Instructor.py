8f71f69068ddda68d777e4112ad6c2d3918075e6
sethtomy <sethtomy@gmail.com>
2019-02-26 17:15:30 -0500

    Delete Instructor.py

--- Utils/Instructor.py
+++
@@ -1,9 +0,0 @@
-from Person import Person
-
-class Instructor(Person):
-
-    def __init__(self, email, first_name, last_name, auth_token):
-        super().__init__(email, first_name, last_name)
-        self.auth_token = auth_token
-        course_list = [1, 2, 3]
-
