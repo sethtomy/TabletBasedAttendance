132ffdbec69da8deb54b2053cd722d75aa4174a0
sethtomy <sethtomy@gmail.com>
2019-02-26 17:15:37 -0500

    Delete Person.py

--- Utils/Person.py
+++
@@ -1,7 +0,0 @@
-class Person:
-
-    #TODO: How do I want to do id's???
-    def __init__(self, email, first_name, last_name):
-        self.email = email
-        self.first_name = first_name
-        self.last_name = last_name
